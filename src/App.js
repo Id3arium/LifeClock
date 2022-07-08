c8241263a29afbd2e110eb5de6f52a55486dbfc1
Alex <andres.a.campos@navy.mil>
2022-07-08 12:52:34 -0700

    prompting user for birth date, adding date picker

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Life Clock</h1>
+      <p>When were you born?</p>
+      <input type='date'></input>
     </div>
   );
 }
