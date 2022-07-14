5241823b7ae0325cd85e9fad1fba243bf2c1f67f
Alex <andres.a.campos@navy.mil>
2022-07-13 18:46:02 -0700

    trying to calculate the times

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,35 +3,50 @@ import React, {useState} from "react";
 import DatePicker from './DatePicker/DatePicker';
 
 function App() {
-  const [birthDate, setBirthDate] = useState(new Date());
+  const [birthDate, setBirthDate] = useState(Date.now());
 
-  function getTimeAlive(date) {
-    let birthDate = date.getTime() 
-    console.log(birthDate)
-    let timeAlive = Date.now() - date.getTime() 
-    return timeAlive
-  }
+  let getSecondsAlive = () => {return (new Date.now - birthDate) / 1000} 
+  let getMinutesAlive = () => { 
+    return (Date.now() - birthDate) / (1000 * 60) 
+  } 
+  let getHoursAlive = () => { 
+    return (Date.now() - birthDate) / (1000 * 60 * 60) 
+  } 
+  let getDaysAlive = () => { 
+    return (Date.now() - birthDate) / (1000 * 60 * 60 * 24) 
+  } 
+  let getWeeksAlive = () => { 
+    return (Date.now() - birthDate) / (1000 * 60 * 60 * 24 * 7) 
+  } 
+  let getMonthsAlive = () => { 
+    let months = (Date.now().getFullYear() - birthDate.getFullYear()) * 12
+    months -= birthDate.getMonth() + 1
+    months += Date.now().getMonth()
+    return months
+  } 
+  let getYearsAlive = () => { 
+    let years = (Date.now().getFullYear() - birthDate.getFullYear())
+    return years
+  } 
 
-  let onDateChanged = (date) => {
-    console.log("date",date)
-    let timeAlive = getTimeAlive(date)
-    console.log("timeAlive",timeAlive)
-    setBirthDate(date)
-  }
+  let onDateChanged = (date) => { setBirthDate(date) }
   return (
     <div className="App">
       <h1>Life Clock</h1>
       <h2> You can't know for certain when you will die. </h2>
       <h3> But I can show you for how long you've lived. </h3>
-      
+
+      <p>whaen were you born? </p>
+
+      <DatePicker onDateChanged={onDateChanged} />
       <span>
-        <p>How many seconds</p>
-        <p>How many minutes</p>
-        <p>How many hours</p>
-        <p>How many days</p>
-        <p>How many weeks</p>
-        <p>How many months</p>
-        <p>How many years</p>
+        <p>How many seconds {getSecondsAlive(birthDate)} </p>
+        <p>How many minutes {getMinutesAlive(birthDate)} </p>
+        <p>How many hours {getHoursAlive(birthDate)} </p>
+        <p>How many days {getDaysAlive(birthDate)} </p>
+        <p>How many weeks {getWeeksAlive(birthDate)} </p>
+        <p>How many months {getMonthsAlive(birthDate)} </p>
+        <p>How many years {getYearsAlive(birthDate)}</p>
         <p>Have passed since that day? </p>
       </span>
 
