2d6ecbc8b75dfc57383d9efefd22123703183a52
Alex <andres.a.campos@navy.mil>
2022-07-13 19:51:51 -0700

    rounding results to one decimal

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,7 +6,7 @@ function App() {
   const [birthDate, setBirthDate] = useState(new Date());
   let getSecondsAlive = () => {return (new Date() - birthDate) / 1000} 
   let getMinutesAlive = () => { 
-    return (new Date() - birthDate) / (1000 * 60) 
+    return (new Date() - birthDate) / (1000 * 60)
   } 
   let getHoursAlive = () => { 
     return (new Date() - birthDate) / (1000 * 60 * 60) 
@@ -19,8 +19,7 @@ function App() {
   } 
   let getMonthsAlive = (now, birthDate ) => { 
     let months = (now.getFullYear() - birthDate.getFullYear()) * 12
-    months -= birthDate.getMonth() + 1
-    months += now.getMonth()
+    months += (now.getMonth() - birthDate.getMonth())
     return months
   } 
   let getYearsAlive = () => { 
@@ -35,17 +34,17 @@ function App() {
       <h2> You can't know for certain when you will die. </h2>
       <h3> But I can show you for how long you've lived. </h3>
 
-      <p>whaen were you born? </p>
-
+      <p>When were you born? </p>
       <DatePicker onDateChanged={onDateChanged} />
+
       <span>
-        <p>How many seconds {getSecondsAlive(birthDate)} </p>
-        <p>How many minutes {getMinutesAlive(birthDate)} </p>
-        <p>How many hours {getHoursAlive(birthDate)} </p>
-        <p>How many days {getDaysAlive(birthDate)} </p>
-        <p>How many weeks {getWeeksAlive(birthDate)} </p>
+        <p>How many seconds {getSecondsAlive().toFixed(1)} </p>
+        <p>How many minutes {getMinutesAlive().toFixed(1)} </p>
+        <p>How many hours {getHoursAlive().toFixed(1)} </p>
+        <p>How many days {getDaysAlive().toFixed(1)} </p>
+        <p>How many weeks {getWeeksAlive().toFixed(1)} </p>
         <p>How many months {getMonthsAlive(new Date(), birthDate)} </p>
-        <p>How many years {getYearsAlive(birthDate)}</p>
+        <p>How many years {getYearsAlive().toFixed(1)}</p>
         <p>Have passed since that day? </p>
       </span>
 
