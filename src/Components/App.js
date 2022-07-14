a206b33cd14793e49960a9943362400535fc2528
Alex <andres.a.campos@navy.mil>
2022-07-13 18:06:47 -0700

    adding the narration text

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,13 +6,14 @@ function App() {
   const [birthDate, setBirthDate] = useState(new Date());
 
   function getTimeAlive(date) {
-    let now = new Date().now
-    console.log()
-    let timeAlive = now - date.getTime() 
+    let birthDate = date.getTime() 
+    console.log(birthDate)
+    let timeAlive = Date.now() - date.getTime() 
     return timeAlive
   }
 
   let onDateChanged = (date) => {
+    console.log("date",date)
     let timeAlive = getTimeAlive(date)
     console.log("timeAlive",timeAlive)
     setBirthDate(date)
@@ -20,10 +21,23 @@ function App() {
   return (
     <div className="App">
       <h1>Life Clock</h1>
-      <p>When were you born?</p>
+      <h2> You can't know for certain when you will die. </h2>
+      <h3> But I can show you for how long you've lived. </h3>
+      
+      <span>
+        <p>How many seconds</p>
+        <p>How many minutes</p>
+        <p>How many hours</p>
+        <p>How many days</p>
+        <p>How many weeks</p>
+        <p>How many months</p>
+        <p>How many years</p>
+        <p>Have passed since that day? </p>
+      </span>
 
-      <DatePicker onDateChanged={onDateChanged}/>
-
+      <p> Think of all the things that have happened in this time. </p>
+      <p> Did things go the way you expected? </p>
+      <p> What will you do next? </p>
 
     </div>
   );
