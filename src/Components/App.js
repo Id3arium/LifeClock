acc51a5d0f5494b5808a1c1451944fb9fa05db23
Alex <andres.a.campos@navy.mil>
2022-08-08 01:08:39 -0700

    trying to get the app to save selected date on page reloads, wip

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,35 +1,46 @@
 import './App.css';
 import React, {useState, useEffect, useLayoutEffect, useRef} from "react";
 import DatePicker from './DatePicker/DatePicker';
+import anime from 'animejs';
 import ClockCard from './ClockCard/ClockCard';
 import * as ch from './ClockHelpers'
-import {animateIntro, animateContent} from "./Animations"
 
 function App() {
   const [birthDate, setBirthDate] = useState(undefined);
   const [time, setTime] = useState(new Date());
   const footerRef = useRef()
   
-  let scrollToElement = (elementRef) => {
-    elementRef.current?.scrollIntoView({ block: "end", behavior: "smooth" })
-  }
-
   const bDay = JSON.parse(localStorage.getItem('birthDate'))
-  if (!bDay) { 
-    let introAnimation = animateIntro() 
-    introAnimation.play()
-  } else {
-    let contentAnimation = animateContent(scrollToElement, footerRef)
-  }
-
   let onDateChanged = (date) => { 
     localStorage.setItem('birthDate', JSON.stringify(date))
     setBirthDate(date)
   }
+
+  let animateIntro = () => { anime.timeline({
+    easing: 'linear',
+  })
+  .add({
+    targets: 'h2, h3',
+    translateY: [-50,0],
+    opacity: [0, 1],
+    delay: anime.stagger(2500, {start:1500}),
+  })
+  .add({
+    targets: '.header > p, .date-picker',
+    opacity: [0, 1],
+    duration: 750,
+    delay: 1500,
+  })
+}
   
   useEffect(() => {
     const interval = setInterval(() => setTime(new Date()), 10);
     console.log("bDay",bDay)
+    if (!bDay) { 
+      animateIntro() 
+    } else {
+      animateContent()
+    }
 
     return () => {
       clearInterval(interval);
@@ -39,6 +50,31 @@ function App() {
   useLayoutEffect( () => {
     if (birthDate) animateContent()
   }, [birthDate])
+ 
+  const scrollToFooter = () => {
+    console.log("scrolling to bottom")
+    footerRef.current?.scrollIntoView({ block: "end", behavior: "smooth" })
+  }
+
+  let animateContent = () => {anime.timeline({  })
+    .add({
+      targets: '.cards-wrapper > *',
+      duration: anime.stagger(100, {from: 'center', start:250, easing: 'linear'}),
+      delay: anime.stagger(100, {from: 'center' , start:250, easing: 'linear'}),
+      translateX: [anime.stagger([500,-500]),0],
+      opacity: [0, 1],
+      easing: 'easeOutQuad',
+    })
+    .add({
+      targets: '.footer > *',
+      translateY: [-25,0],
+      opacity: [0, 1],
+      delay: anime.stagger(3500, {start:10000}),
+      easing: 'easeOutQuad',
+      changeBegin: (anim) => {
+        scrollToFooter()
+    }
+  })}
 
   return (
     <div className="App" >
@@ -50,6 +86,7 @@ function App() {
         <p>When were you born? </p>
         <DatePicker 
           onDateChanged={onDateChanged} 
+          defaultActiveStartDate={bDay ? new Date(bDay) : new Date()}
         />
       </div>
 
